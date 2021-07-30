ebde0454162bcf029c67f67576648fb9dda5c95a
nadimnesar <nadimahmednesar@gmail.com>
2021-07-31 00:27:10 +0600

    ininital final

--- app/src/main/java/com/example/livecalc/MainActivity.java
+++ app/src/main/java/com/example/livecalc/MainActivity.java
@@ -36,11 +36,9 @@ public class MainActivity extends AppCompatActivity {
     public static double eval(final String str) {
         return new Object() {
             int pos = -1, ch;
-
             void nextChar() {
                 ch = (++pos < str.length()) ? str.charAt(pos) : -1;
             }
-
             boolean eat(int charToEat) {
                 while (ch == ' ') nextChar();
                 if (ch == charToEat) {
@@ -81,27 +79,15 @@ public class MainActivity extends AppCompatActivity {
 
                 double x;
                 int startPos = this.pos;
-                if (eat('(')) { // parentheses
+                if (eat('(')) {
                     x = parseExpression();
                     eat(')');
                 } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
                     while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
                     x = Double.parseDouble(str.substring(startPos, this.pos));
-                } else if (ch >= 'a' && ch <= 'z') { // functions
-                    while (ch >= 'a' && ch <= 'z') nextChar();
-                    String func = str.substring(startPos, this.pos);
-                    x = parseFactor();
-                    if (func.equals("sqrt")) x = Math.sqrt(x);
-                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
-                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
-                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
-                    else if (func.equals("log")) x = Math.log10(x);
-                    else if (func.equals("ln")) x = Math.log(x);
-                    else throw new RuntimeException("Unknown function: " + func);
-                } else {
-                    throw new RuntimeException("Unexpected: " + (char)ch);
                 }
-                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+                else throw new RuntimeException("Unexpected: " + (char)ch);
+                if (eat('^')) x = Math.pow(x, parseFactor());
                 return x;
             }
         }.parse();
